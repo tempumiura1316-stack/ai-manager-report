91332782cea3dd38d9d291365800d8f72fa4170f
tempumiura1316-stack <tempu.miura.1316@gmail.com>
2025-10-08 17:07:49 +0900

    PDF4,5歩目 app.py

--- app.py
+++ app.py
@@ -21,3 +21,5 @@ if st.button("週報を作成"):
 buf = io.BytesIO()
 doc = SimpleDocTemplate(buf, pagesize=A4, leftMargin=36, rightMargin=36, topMargin=36, bottomMargin=36)
 pdfmetrics.registerFont(UnicodeCIDFont("HeiseiMin-W3"))
+styles = getSampleStyleSheet()
+normal_style = ParagraphStyle(name="NormalJP", parent=styles["Normal"], fontName="HeiseiMin-W3", fontSize=11, leading=16)
