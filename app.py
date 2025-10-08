b5bcf64aaa436132de7dcc53079077b0ab3ee983
tempumiura1316-stack <tempu.miura.1316@gmail.com>
2025-10-08 15:38:09 +0900

    クライアント app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 
-client = OpenAI() #OpenAIのAPIと通信するためのクライアント(接続口)
-
+client = OpenAI()
 
 st.set_page_config(page_title="AI報告書メーカー", page_icon="🧠")
 st.title("🧠 AI報告書メーカー（テスト版）")
