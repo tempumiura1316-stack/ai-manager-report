bafb47416f5c731eb63baae1d705ab501fcdf4a1
tempumiura1316-stack <tempu.miura.1316@gmail.com>
2025-10-09 16:22:07 +0900

    テンプレ

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ if purpose_option == "その他（自由入力）":
 else:
     purpose = purpose_option
 
-template = st.text_area("🧩 報告書テンプレ（任意）", placeholder="例：\n① 今週の成果\n② 課題\n③ 来週の方針\n④ 一言コメント", height=150)
+template = st.text_area("報告書テンプレ（任意）", placeholder="例：\n① 今週の成果\n② 課題\n③ 来週の方針\n④ 一言コメント", height=150)
 
 #AIで実行
 if st.button("週報を作成"):
