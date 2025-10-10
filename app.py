f96a36e5284b37108512ebb87513e1fa4010fd3b
tempumiura1316-stack <tempu.miura.1316@gmail.com>
2025-10-10 16:16:38 +0900

    多言語

--- app.py
+++ app.py
@@ -35,6 +35,9 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+LANG_CHOICES = [("日本語","ja"),("English","en"),("简体中文","zh-CN"),("繁體中文","zh-TW"),("한국어","ko"),("Français","fr"),("Deutsch","de"),("Español","es"),("Português (Brasil)","pt-BR"),("Italiano","it"),("Русский","ru"),("العربية (PDF非推奨)","ar"),("हिन्दी","hi"),("Bahasa Indonesia","id"),("ไทย","th"),("Tiếng Việt","vi"),("Türkçe","tr"),("Nederlands","nl"),("Polski","pl"),("Українська","uk")]
+
+
 st.write("こんにちは！これは週報作成用のアプリです。")
 
 text = st.text_area("日報を入力してください")
